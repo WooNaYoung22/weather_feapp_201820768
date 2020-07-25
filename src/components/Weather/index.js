5086a660e320b92d75b6f2b960f06afd8398790b
nyw <skdud@ajou.ac.kr>
2020-07-26 02:19:10 +0900

    print json

--- src/components/Weather/index.js
+++ src/components/Weather/index.js
@@ -5,32 +5,53 @@ import { render } from "@testing-library/react";
 //const API_WEATHER = 'http://localhost:8888/weather-service/weathers?cityName=${cityName}';
 
 // Stateless component
-const Weather = (props) => {
-//class Weather extends React.Component {
-  // 1 .
-  // class 형태로 변경 후 fetch 선택 도시의 날씨
+//const Weather = (props) => {
+//class
+class Weather extends React.Component {
 
-  // 2 .
-  // react Hook 
-  // state, setState
+  state = {
+    weather: "",
+    weather_description: "",
+    temperature: 0,
+  }
 
-  const { cityName } = props.match.params;
-  console.log(cityName);
-  
-  const weatherData = fetch(`http://localhost:8888/weather-service/weathers?cityName=${cityName}`)
-    .then((res) => res.json())
-    .then((json) => console.log(json));
-  
-  console.log(weatherData);
+  componentDidMount() {
 
-  return (
-    <div>
-      <h1>Weather : {cityName}</h1>
-      <p>Here is front page.!</p>
+    //const { cityName } = props.match.params;
+    const { cityName } = this.props.match.params;
+    console.log(cityName);
+    const { weather } = this.state;
+    const { weather_description } = this.state;
+    const { temperature } = this.state;
+    
+    const weatherData = fetch(`http://localhost:8888/weather-service/weathers?cityName=${cityName}`)
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({
+          weather: data.weather[0].main,
+          weather_description: data.weather[0].description,
+          temperature: (data.main.temp-273.15).toFixed(2),
+        });
+      })
+      .then().catch();
+      //.catch(e => console.log(e));
+  }
 
-    </div>
-  );
+  render() {
+    const { cityName } = this.props.match.params;
+    const { weather, weather_description, temperature } = this.state;
 
+    return (
+      <div>
+        <p>Here is front page.!</p>
+        <h1>City name - {cityName}</h1>
+        <h1>Weather - {weather}</h1>
+        <h1>Weather details - {weather_description}</h1>
+        <h1>Temperature - {temperature}</h1>
+
+      </div>
+    );
+  }
 };
 
 export default Weather;
